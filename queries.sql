785be576727c8cded39f4708dc21a71b3798f6d5
Riscanto <victor.kostinsky@gmail.com>
2024-06-30 22:41:47 +0300

    Add files via upload

--- queries.sql
+++ queries.sql
@@ -1,9 +1,13 @@
-
+-- Задание 4
+--Подсчёт числа уникальних значений customer_id в таблице customers.
 select COUNT(distinct customer_id) as customers_count
 from
     customers;
-   
-   select
+
+--Задание 5.1. 
+--Выводит 10 продавцов с наибольшей выручкой (Имя-фамилия, количество продаж, выручка). Сортировка по убыванию выручки.
+
+select
     CONCAT(e.first_name, ' ', e.last_name) as seller,
     COUNT(s.sales_id) as operations,
     FLOOR(SUM(quantity * price)) as income
@@ -15,6 +19,10 @@ left join employees as e
 group by seller
 order by income desc
 limit 10;
+
+--Задание 5.2. 
+--Выводит всех продавцов, чья выручка ниже средней выручки всех продавцов (Не средней суммы продажи или средней выручки по всей компании, это разные суммы.)
+--(Имя-фамилия, средняя выручка за сделку). Сортировка по общей выручке продавца (в таблице не отражена)
 
 with avg_sales as (
     select
@@ -37,6 +45,10 @@ from
     avg_sales
 where average_income < (select AVG(average_income) from avg_sales)
 order by average_income asc;
+
+--Задание 5.3. 
+--Выводит суммарные продажи каждого продавца в определённый день недели. Напр.:Сколько всего Джон продал за все понедельники.
+--(Имя-фамилия, день недели, выручка). Сортировка по порядковому дню недели и продавцу.
 
 with day_of_week_income as (
     select
@@ -61,6 +73,9 @@ left join employees as e
     on day_of_week_income.sales_person_id = e.employee_id
 order by day_num, seller;
 
+--Задание 6.1.
+--Запрос выводит 3 возрастные группы: 16-25 лет, 26-40 и 40+ и количество покупателей в каждой из них
+
 select
     case
         when age between 16 and 25 then '16-25'
@@ -71,6 +86,10 @@ select
 from customers
 group by age_category
 order by age_category;
+
+--Задание 6.2.
+--Запрос выводит данные о количестве уникальных покупателей в каждом месяце и выручке, которую они принесли.
+--(месяц в формате YYYY-MM, число уникальных покупателей, сумма выручки). Сортировка по дате по возрастанию.
 
 select
     total_customers,
@@ -87,7 +106,10 @@ from
         on s.product_id = p.product_id
     group by selling_month
     order by selling_month asc) as t;
-
+ 
+--Задание 6.3.
+--Запрос выводит перечень покупателей, чья первая покупка была за 0 рублей, т.е. по акции, дату первой покупки и продавца.
+--(Имя-фамилия покупателя, дата первой покупки, имя-фамилия продавца) Сортировка по customer_id, индивидуальному идентификатору покупателя.
 
 with tab as (
     select
@@ -114,4 +136,3 @@ left join
 where price = 0 and sale_date = min_date
 group by customer, seller, s.customer_id
 order by s.customer_id;
-   
