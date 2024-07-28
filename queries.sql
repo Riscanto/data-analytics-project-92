f3f5571e911e1ea9e75d3d979f5ca5e8de9711d9
Riscanto <victor.kostinsky@gmail.com>
2024-07-28 20:02:03 +0300

    Add files via upload

--- queries.sql
+++ queries.sql
@@ -18,8 +18,8 @@ left join products as p
     on s.product_id = p.product_id
 left join employees as e
     on s.sales_person_id = e.employee_id
-group by seller
-order by income desc
+group by 1
+order by 3 desc
 limit 10;
 
 --Задание 5.2. 
@@ -40,7 +40,7 @@ with avg_sales as (
         on s.product_id = p.product_id
     left join employees as e
         on s.sales_person_id = e.employee_id
-    group by seller
+    group by 1
 )
 
 select
@@ -49,7 +49,7 @@ select
 from
     avg_sales
 where average_income < (select AVG(average_income) from avg_sales)
-order by average_income asc;
+order by 2 asc;
 
 --Задание 5.3. 
 --Выводит суммарные продажи каждого продавца в определённый день недели. 
@@ -68,18 +68,18 @@ with day_of_week_income as (
     from sales as s
     left join products as p
         on s.product_id = p.product_id
-    group by s.sales_person_id, day_num, day_name
-    order by day_num, s.sales_person_id
+    group by 1, 2, 3
+    order by 2, 1
 )
 
 select
     day_of_week_income.day_name as day_of_week,
     day_of_week_income.income,
-    CONCAT(e.first_name, ' ', e.last_name) as seller
+    e.first_name||' '||e.last_name as seller
 from day_of_week_income
 left join employees as e
     on day_of_week_income.sales_person_id = e.employee_id
-order by day_of_week_income.day_num, seller;
+order by day_of_week_income.day_num, 3;
 
 --Задание 6.1.
 --Запрос выводит 3 возрастные группы: 16-25 лет, 26-40 и 40+
@@ -93,8 +93,8 @@ select
     end as age_category,
     COUNT(customer_id) as age_count
 from customers
-group by age_category
-order by age_category;
+group by 1
+order by 1;
 
 --Задание 6.2.
 --Запрос выводит данные о количестве уникальных покупателей в каждом месяце
@@ -115,8 +115,8 @@ from
     left join
         products as p
         on s.product_id = p.product_id
-    group by selling_month
-    order by selling_month asc) as t;
+    group by 1
+    order by 1 asc) as t;
 
 --Задание 6.3.
 --Запрос выводит перечень покупателей, чья первая покупка была за 0 рублей,
@@ -124,19 +124,10 @@ from
 --(Имя-фамилия покупателя, дата первой покупки, имя-фамилия продавца)
 -- Сортировка по customer_id, индивидуальному идентификатору покупателя.
 
-with tab as (
-    select
-        customer_id,
-        MIN(sale_date) as min_date
-    from
-        sales
-    group by customer_id
-)
-
-select
-    CONCAT(c.first_name, ' ', c.last_name) as customer,
+select distinct on (customer)
+    c.first_name||' '||c.last_name as customer,
     MIN(s.sale_date) as sale_date,
-    CONCAT(e.first_name, ' ', e.last_name) as seller
+    e.first_name||' '||e.last_name as seller
 from sales as s
 left join customers as c
     on s.customer_id = c.customer_id
@@ -144,9 +135,6 @@ left join employees as e
     on s.sales_person_id = e.employee_id
 left join products as p
     on s.product_id = p.product_id
-left join
-    tab
-    on c.customer_id = tab.customer_id
-where p.price = 0 and sale_date = tab.min_date
-group by customer, seller, s.customer_id
-order by s.customer_id;
+where p.price = 0 
+group by 1, 3, s.customer_id
+order by customer;
